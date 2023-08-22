93d0885b72daea26cf27cf3b2e27a3674ba5bdb2
Faizwaseem <136327727+Faizwaseem@users.noreply.github.com>
2023-08-22 14:16:32 -0700

    firstcommit

---
+++ style.js
@@ -0,0 +1,45 @@
+//Q.01
+
+// let names = ['Faiz','ahsan','hamza','aliyan'] ;
+
+// names.push('moiz');
+
+
+
+// console.log(names);
+
+
+// // Q.02
+// let names = ['Faiz','ahsan','hamza','aliyan'] ;
+
+// names.splice(1, 1,'asad', 'usama');
+// console.log(names);
+
+// // Q-3
+
+// let names = ['Faiz','ahsan','hamza','aliyan'] ;
+
+//  let hide= names.slice(3)
+
+ 
+// console.log(hide);
+
+//Q.04
+
+// let names=['faiz','hassan','moiz']
+
+// names.push('helen','grace')
+
+// console.log(names)
+
+//Q.05
+
+// let names = ['Faiz','ahsan','hamza','aliyan'] ;
+
+// names.splice(3,3,'moiz')
+
+
+// console.log(names)
+
+
+
